0ee35795abb75f91ece1f91d08b1beeeb6df6725
Alumnos <Alumnos@PC190>
2023-05-12 12:38:04 -0300

    Agregar archivos de proyecto.

---
+++ prySALAS_SP1_EPR/Principal.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace prySALAS_SP1_EPR
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.mrcDatos = new System.Windows.Forms.GroupBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblModulo = new System.Windows.Forms.Label();
+            this.lstModulo = new System.Windows.Forms.ComboBox();
+            this.cmdAceptar = new System.Windows.Forms.Button();
+            this.cmdCancelar = new System.Windows.Forms.Button();
+            this.mrcDatos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mrcDatos
+            // 
+            this.mrcDatos.Controls.Add(this.lstModulo);
+            this.mrcDatos.Controls.Add(this.lblModulo);
+            this.mrcDatos.Controls.Add(this.txtPassword);
+            this.mrcDatos.Controls.Add(this.lblPassword);
+            this.mrcDatos.Controls.Add(this.txtUsuario);
+            this.mrcDatos.Controls.Add(this.lblUsuario);
+            this.mrcDatos.Location = new System.Drawing.Point(12, 12);
+            this.mrcDatos.Name = "mrcDatos";
+            this.mrcDatos.Size = new System.Drawing.Size(283, 161);
+            this.mrcDatos.TabIndex = 0;
+            this.mrcDatos.TabStop = false;
+            this.mrcDatos.Text = "Datos";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(17, 25);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(71, 20);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(143, 25);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(120, 20);
+            this.txtUsuario.TabIndex = 1;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(17, 74);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(102, 20);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Contraseña";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(143, 74);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(120, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // lblModulo
+            // 
+            this.lblModulo.AutoSize = true;
+            this.lblModulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblModulo.Location = new System.Drawing.Point(17, 119);
+            this.lblModulo.Name = "lblModulo";
+            this.lblModulo.Size = new System.Drawing.Size(67, 20);
+            this.lblModulo.TabIndex = 4;
+            this.lblModulo.Text = "Módulo";
+            // 
+            // lstModulo
+            // 
+            this.lstModulo.FormattingEnabled = true;
+            this.lstModulo.Location = new System.Drawing.Point(142, 118);
+            this.lstModulo.Name = "lstModulo";
+            this.lstModulo.Size = new System.Drawing.Size(121, 21);
+            this.lstModulo.TabIndex = 5;
+            // 
+            // cmdAceptar
+            // 
+            this.cmdAceptar.Location = new System.Drawing.Point(299, 18);
+            this.cmdAceptar.Name = "cmdAceptar";
+            this.cmdAceptar.Size = new System.Drawing.Size(141, 50);
+            this.cmdAceptar.TabIndex = 1;
+            this.cmdAceptar.Text = "Aceptar";
+            this.cmdAceptar.UseVisualStyleBackColor = true;
+            // 
+            // cmdCancelar
+            // 
+            this.cmdCancelar.Location = new System.Drawing.Point(299, 74);
+            this.cmdCancelar.Name = "cmdCancelar";
+            this.cmdCancelar.Size = new System.Drawing.Size(141, 50);
+            this.cmdCancelar.TabIndex = 2;
+            this.cmdCancelar.Text = "Cancelar";
+            this.cmdCancelar.UseVisualStyleBackColor = true;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 183);
+            this.Controls.Add(this.cmdCancelar);
+            this.Controls.Add(this.cmdAceptar);
+            this.Controls.Add(this.mrcDatos);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPrincipal";
+            this.Text = "Login";
+            this.mrcDatos.ResumeLayout(false);
+            this.mrcDatos.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox mrcDatos;
+        private System.Windows.Forms.ComboBox lstModulo;
+        private System.Windows.Forms.Label lblModulo;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Button cmdAceptar;
+        private System.Windows.Forms.Button cmdCancelar;
+    }
+}
